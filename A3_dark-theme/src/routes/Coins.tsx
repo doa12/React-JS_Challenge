ae1deb7496ad77ad8cd0054eefd4681265e8ecf4
Heen <agny_@naver.com>
2022-09-10 06:43:30 +0900

    :tada: ReactJS Challenge Start!

---
+++ A3_dark-theme/src/routes/Coins.tsx
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+import Link from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`
+
+const Header = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const CoinsList = styled.ul``
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${props => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    /* 글씨 밖까지 클릭되게 하기 위해서 block 처리 */
+    /* display: block; */
+  }
+  &:hover {
+    a {
+      color: ${props =>props.theme.accentColor};
+    }
+  }
+`
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`
+
+interface CoinInterface {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async() => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      // setLoading(false);
+    })();
+  }, [])
+  console.log(coins);
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>;
+      </Header>
+      {loading ? (
+          <Loader>Loading...</Loader>
+        ) : (
+          <CoinsList>
+            {/* rarr: 오른쪽 방향의 화살표 삽입 */}
+            {coins.map(coin => (
+              <Coin key={coin.id}>
+                <Link to={{
+                    pathname: `/${coin.id}`,
+                    state: {name: coin.name},
+                  }}>
+                  <Img src={`https://cryptoicon-api.vercel.app/${coin.symbol.toLowerCase()}`} />
+                  {coin.name} &rarr
+                </Link>
+              </Coin>
+            ))}
+          </CoinsList>
+        )
+      }
+    </Container>
+  )
+}
+
+export default Coins;
---
+++ A3_dark-theme/src/theme.ts
@@ -0,0 +1,8 @@
+import {DefaultTheme} from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#dcdde1",
+  // #718093
+  accentColor: "#8c7ae6",
+};
